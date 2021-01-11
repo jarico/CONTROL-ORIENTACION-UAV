9b4e0ffbd761cbfe8d96f00314ca7852bbe594c3
jarico <javier.rico@unirioja.es>
2021-01-11 17:18:10 +0100

    Add files via upload

---
+++ CIC2020/CIC2020_F1C2/UAV_sf.c
@@ -0,0 +1,13 @@
+/*
+ * Generated S-function Target for model UAV. 
+ * This source file provides access to the generated S-function target
+ * for other models.
+ *
+ * Created: Tue Dec 10 13:17:33 2019
+ */
+
+#include "UAV_sf.h"
+#include "UAV_sfcn_rtw\UAV_sf.c"
+#include "UAV_sfcn_rtw\UAV_sf_data.c"
+
+
---
+++ CIC2020/CIC2020_F1C2/UAV_sf.h
@@ -0,0 +1,15 @@
+/*
+* Generated S-function Target for model UAV. 
+* This file provides access to the generated S-function target
+* export file for other models.
+*
+* Created: Tue Dec 10 13:17:33 2019
+*/
+
+#ifndef RTWSFCN_UAV_sf_H
+#define RTWSFCN_UAV_sf_H
+
+#include "UAV_sfcn_rtw\UAV_sf.h"
+  #include "UAV_sfcn_rtw\UAV_sf_private.h"
+
+#endif
