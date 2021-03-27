f9b7267d4b3ed6a28284fb6af2a20a998192f2f0
jarico <javier.rico@unirioja.es>
2021-03-27 10:59:02 +0100

    Añadidas las  versiones para Linux

---
+++ CIC2021/Simulador/Linux/CIC2021_Simulador_Linux__Matlab2018b/UAV_sf.h
@@ -0,0 +1,15 @@
+/*
+* Generated S-function Target for model UAV. 
+* This file provides access to the generated S-function target
+* export file for other models.
+*
+* Created: Mon Mar 22 20:38:45 2021
+*/
+
+#ifndef RTWSFCN_UAV_sf_H
+#define RTWSFCN_UAV_sf_H
+
+#include "UAV_sfcn_rtw/UAV_sf.h"
+  #include "UAV_sfcn_rtw/UAV_sf_private.h"
+
+#endif
