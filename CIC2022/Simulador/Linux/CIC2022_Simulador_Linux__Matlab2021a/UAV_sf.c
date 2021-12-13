462db0ca41394782c11bffc00790ac26da91fbf7
jarico <javier.rico@unirioja.es>
2021-12-13 11:49:03 +0100

    Archivos 2022

---
+++ CIC2022/Simulador/Linux/CIC2022_Simulador_Linux__Matlab2021a/UAV_sf.c
@@ -0,0 +1,13 @@
+/*
+ * Generated S-function Target for model UAV. 
+ * This source file provides access to the generated S-function target
+ * for other models.
+ *
+ * Created: Sat Dec 11 19:10:55 2021
+ */
+
+#include "UAV_sf.h"
+#include "UAV_sfcn_rtw/UAV_sf.c"
+#include "UAV_sfcn_rtw/UAV_sf_data.c"
+
+
---
+++ CIC2022/Simulador/Mac OS X/CIC2022_Simulador_Mac__Matlab2018b/UAV_sf.h
@@ -0,0 +1,15 @@
+/*
+* Generated S-function Target for model UAV. 
+* This file provides access to the generated S-function target
+* export file for other models.
+*
+* Created: Sat Dec 11 19:24:53 2021
+*/
+
+#ifndef RTWSFCN_UAV_sf_H
+#define RTWSFCN_UAV_sf_H
+
+#include "UAV_sfcn_rtw/UAV_sf.h"
+  #include "UAV_sfcn_rtw/UAV_sf_private.h"
+
+#endif
